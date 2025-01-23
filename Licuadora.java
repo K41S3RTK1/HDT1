748974bde915c0c219d2abd814ba82b0b89dd1b4
Sandoval <gtkaiser29@gmail.com>
2025-01-23 11:36:31 -0600

    Código Interfaz
    Se agrego el código de la interfaz para la compatibilidad con otros grupos.

--- Licuadora.java
+++ Licuadora.java
@@ -1,1 +1,11 @@
 // Interfaz Licuadora
+public interface Licuadora {
+
+      boolean encenderLicuadora();
+      boolean apagarLicuadora();
+      boolean llenarLicuadora();
+      boolean vaciarLicuadora();
+      boolean incrementarVelocidad(int velocidad);
+      int obtenerVelocidadActual();
+      boolean estaLlena();
+  }
