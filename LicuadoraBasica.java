1bb5140f1f49555fbafd1bf8a1380b2f84b4f921
JCova10 <ova24701@uvg.edu.gt>
2025-01-23 20:43:59 -0600

    Update LicuadoraBasica.java

--- LicuadoraBasica.java
+++ LicuadoraBasica.java
@@ -9,4 +9,62 @@ public class LicuadoraBasica implements Licuadora {
         this.llena = false;
         this.velocidadActual = 0;
     }
+        @Override
+    public boolean encenderLicuadora() {
+        if (!encendida) {
+            encendida = true;
+            return true;
+        }
+        return false;
+    }
+
+     @Override
+    public boolean apagarLicuadora() {
+        if (encendida) {
+            encendida = false;
+            velocidadActual = 0; // Restablece la velocidad al apagar
+            return true;
+        }
+        return false;
+    }
+
+      @Override
+    public boolean llenarLicuadora() {
+        if (!llena) {
+            llena = true;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean vaciarLicuadora() {
+        if (llena) {
+            llena = false;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean incrementarVelocidad(int velocidad) {
+        if (encendida && llena && velocidad == velocidadActual + 1 && velocidad <= 10) {
+            velocidadActual = velocidad;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int obtenerVelocidadActual() {
+        return velocidadActual;
+    }
+
+    @Override
+    public boolean estaLlena() {
+        return llena;
+    }
+}
+
+
 }
