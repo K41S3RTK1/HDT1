f261da9738123cffafe97d532b7eff20d249a06f
Mora108 <sebasmoralesh2001@gmail.com>
2025-01-23 19:42:45 -0600

    implementación de interfaz

--- LicuadoraBasica.java
+++ LicuadoraBasica.java
@@ -1,3 +1,12 @@
-public class LicuadoraBasica {
-      
+      // Implementación de la interfaz Licuadora
+public class LicuadoraBasica implements Licuadora {
+    private boolean encendida;
+    private boolean llena;
+    private int velocidadActual;
+
+    public LicuadoraBasica() {
+        this.encendida = false;
+        this.llena = false;
+        this.velocidadActual = 0;
+    }
 }
